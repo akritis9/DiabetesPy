8d0153140b745cfd4db81dbbc34a3a145d54add5
akritis9 <58141986+akritis9@users.noreply.github.com>
2021-01-30 22:12:16 -0800

    imports

--- diabetes.py
+++ diabetes.py
@@ -5,4 +5,28 @@ Created on Sat Jan 30 21:40:44 2021
 
 @author: akritisharma
 """
+# imports
+import pandas as pd
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
+# load data
+df = pd.read_csv("diabetes.csv", low_memory = False)
+
+# view vriables
+varlist = list(df.columns)
+# ['Pregnancies',
+#  'Glucose',
+#  'BloodPressure',
+#  'SkinThickness',
+#  'Insulin',
+#  'BMI',
+#  'DiabetesPedigreeFunction',
+#  'Age',
+#  'Outcome']
+
+# set Outcome as the dependent variable
+
+y = df['Outcome']
